5fb0c962cfd45f87254d15a761c04de96704ad3d
unknown <slavko.vuletic92@gmail.com>
2021-11-10 00:15:05 +0100

    jest passing tests

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const v_to_md5 = (inVal = null) => {
-  try {
-    console.log(JSON.stringify(inVal));
-    return true;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-};
+const v_to_md5 = require('./source/v_to_md5');
 
 module.exports = v_to_md5;
