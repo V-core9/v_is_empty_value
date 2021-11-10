55fd962de6b0db85a6f3e1078bea83de43ef8b4b
unknown <slavko.vuletic92@gmail.com>
2021-11-10 01:58:28 +0100

    yea more small polishing to have better coding standard

--- README.md
+++ README.md
@@ -5,11 +5,9 @@ Install using command :
     npm install v_to_md5 --save
 
 How to use: 
-
   
     const v_to_md5 = require("v_to_md5");
 
     console.log( v_to_md5("demo_password_123456") )
     
 //-> returns hash : b9a0dc00aff156a2a5e915ba00a590ba
-
--- source/v_to_md5.js
+++ source/v_to_md5.js
@@ -1,10 +1,9 @@
 var crypto = require("crypto");
 
-v_to_md5 = (inVal = null) => {
+const v_to_md5 = (inVal = null) => {
   try {
     return crypto.createHash("md5").update(inVal).digest("hex");
   } catch (error) {
-    console.log(error);
     return false;
   }
 };
