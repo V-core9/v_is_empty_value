bfbb2d7c02f954efa6f4cdbd57f4c9facc3579e3
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-06 14:48:41 +0100

    yea some fixes for codacy

--- source/index.js
+++ source/index.js
@@ -1,15 +1,18 @@
 //? Function that checks if the value is empty
-v_is_empty = async (value) => {
-  //! Returns false if it's instance of Date Object
-  if (value instanceof Date) return false;
-  //! Returns true if Array or Object is empty
-  if (typeof value === 'object' && value !== null) return (Object.keys(value) === null || Object.keys(value).length === 0);
+const v_is_empty = async (value) => {
+  if (value instanceof Date) {
+    //! Returns false if it's instance of Date Object
+    return false;
+  } else if (typeof value === "object" && value !== null) {
+    //! Returns true if Array or Object is empty
+    return (Object.keys(value) === null || Object.keys(value).length === 0);
+  }
   //! Returns true if value is null or undefined or empty string
   return (!value ? true : false);
 };
 
 //? Function that will check if value is not empty
-v_is_not_empty = async (value) => !(await v_is_empty(value));
+const v_is_not_empty = async (value) => !(await v_is_empty(value));
 
 module.exports = {
   isEmpty: v_is_empty,
