54e4743b3c86d15e9f9e5fcfed879060cc46f2df
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-09-19 10:34:41 +0200

    added benchmark script for sync and async version of the test

---
+++ __tests__/JEST/async_benchmark.js
@@ -0,0 +1,25 @@
+const { isEmptySync, notEmptySync } = require("../../source");
+
+const itemCount = 1000000;
+
+test('benchmarking isEmptySync', () => {
+  const start = Date.now();
+  for (let i = 0; i < itemCount; i++) {
+    isEmptySync(i);
+  }
+  const average = (1 / ((Date.now() - start) / itemCount));
+  console.log(average);
+
+  expect(average).toBeGreaterThan(500); //? Items Per Millisecond
+});
+
+test('benchmarking notEmptySync', () => {
+  const start = Date.now();
+  for (let i = 0; i < itemCount; i++) {
+    notEmptySync(i);
+  }
+  const average = (1 / ((Date.now() - start) / itemCount));
+  console.log(average);
+
+  expect(average).toBeGreaterThan(500); //? Items Per Millisecond
+});
