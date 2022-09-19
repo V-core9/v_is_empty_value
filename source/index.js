ba513578261b722192811f97cb27234674a9b6ce
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-09-19 10:52:46 +0200

    yea nice ammount of performance gained from this small rework...also changed to esversion 11

--- source/index.js
+++ source/index.js
@@ -1,14 +1,21 @@
 const empty = (value) => {
-  if (value instanceof Date || value instanceof Promise || value instanceof Error || typeof value === 'boolean') {
-    //! Returns false if it's instance of Date Object
-    return false;
-  } else if (typeof value === "object" && value !== null) {
-    //! Returns true if Array or Object is empty
-    return (Object.keys(value) === null || Object.keys(value).length === 0);
-  }
+  const vType = typeof value;
+
+  const vInst = value?.constructor?.name || null;
+  // console.log(vInst);
+
+  //! Returns false if it's instance of Date, Promise, Error or typeof Boolean
+  if (['Date', 'Promise', 'Error'].indexOf(vInst) !== -1 || vType === 'boolean') return false;
+  //if (value instanceof Date || value instanceof Promise || value instanceof Error || vType === 'boolean') return false;
+
+  //! Returns true if Array or Object is empty
+  if (vType === "object" && value !== null) return (Object.keys(value) === null || Object.keys(value).length === 0);
+
   //! Returns true if value is null or undefined or empty string
   return (!value);
+
 };
+
 
 const v_is_empty_value = {
 
@@ -19,20 +26,15 @@ const v_is_empty_value = {
   //? Not Empty Checker
   notEmpty: async (value) => !empty(value),
 
-
   //* Synchronous functions
   //? isEmpty Checker
-  isEmptySync: function (value) {
-    return empty(value);
-  },
+  isEmptySync: (value) => empty(value),
 
   //? Not Empty Checker
-  notEmptySync: function (value) {
-    return !empty(value);
-  }
-
+  notEmptySync: (value) => !empty(value),
 
 };
 
 module.exports = v_is_empty_value;
+
 module.exports.default = v_is_empty_value;
